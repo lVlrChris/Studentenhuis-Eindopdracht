5ae38c8e4af6886edef1f0d5c94a75b0b1c56f66
Chris <christian-boer@hotmail.com>
2018-05-16 00:08:43 +0200

    Studentenhuis route

--- routes/routes_apiv1_maaltijd.js
+++ routes/routes_apiv1_maaltijd.js
@@ -3,11 +3,11 @@ const router = express.Router;
 const maaltijd_manager = require("../managers/maaltijd_manager");
 
 // hier schrijven we router endpoints
-// router.get('/api/studentenhuis/:huisId/maaltijd', maaltijd_manager.getMaaltijd);
-// router.post('/', maaltijd_controller.createMaaltijd);
-// router.get('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.getMaaltijdById);
-// router.put('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.putMaaltijd);
-// router.delete('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.deleteMaaltijd);
+// router.post('studentenhuis/:huidId/maaltijd', maaltijd_manager.createMaaltijd);
+// router.get('studentenhuis/:huisId/maaltijd', maaltijd_manager.getMaaltijd);
+// router.get('studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.getMaaltijdById);
+// router.put('studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.putMaaltijd);
+// router.delete('studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_manager.deleteMaaltijd);
 
 module.exports = router;
 
