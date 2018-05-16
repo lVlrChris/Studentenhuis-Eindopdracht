95fdc3e85ff7751664d213ddfd51a584cfa26a65
Chris <christian-boer@hotmail.com>
2018-05-16 23:33:16 +0200

    createMaaltijd

--- managers/maaltijd_manager.js
+++ managers/maaltijd_manager.js
@@ -1,4 +1,6 @@
 const maaltijd = require('../domain/Maaltijd');
+const auth = require("../auth/authentication");
+const db = require("../datasource/db");
 
 let maaltijdList = [];
 
@@ -7,8 +9,82 @@ let maaltijdList = [];
 module.exports = {
     createMaaltijd(req, res, next){
         console.log('createMaaltijd was called');
-        res.status(200).end();
+
+        const name = req.body.naam || "";
+        const desc = req.body.beschrijving || "";
+        const ingr = req.body.ingredienten || "";
+        const allergy = req.body.allergie || "";
+        const price = req.body.prijs || "";
+        const huisId = req.huisId || "";
+        const token = req.header("X-Access-Token") || "";
+        let userId = "";
+
+        auth.decodeToken(token, (err, payload) => {
+            if (err) {
+                next(err);
+            } else {
+                userId = payload.sub;
+            }
+        });
+
+        new Promise(function (resolve) {
+            //Check if house exists
+            let houseExists = false;
+
+            let selectQuery = {
+                sql: "SELECT * FROM studentenhuis " +
+                "WHERE ID = '" + huisId + "';"
+            };
+
+            db.query(selectQuery, (error, result) => {
+                if (error) {
+                    next(error);
+                } else {
+                    if (result.length > 0) {
+                        houseExists = true;
+                        //TODO: make maaltijd object with verifications (412)
+                        resolve(houseExists);
+                    } else {
+                        console.log("No houses found");
+                        res.status(404).json(new ApiError("Geen studentenhuis gevonden met ID: " + huisId, 404))
+                    }
+                }
+            });
+
+
+        }).then(function (result) {
+
+            //Create maaltijd
+            if(result) {
+                let insertQuery = {
+                    sql: "INSERT INTO maaltijd (Naam, Beschrijving, Ingredienten, Allergie, Prijs, UserID, StudentenhuisID) " +
+                    "VALUES ('" + name + "', " +
+                    "'" + desc + "', " +
+                    "'" + ingr + "', " +
+                    "'" + allergy + "', " +
+                    "'" + price + "', " +
+                    "'" + userId + "', " +
+                    "'" + huisId + "');"
+                };
+
+                db.query(insertQuery, function (error, result) {
+                    if (error) {
+                        next(error);
+                    } else {
+                        console.log("Maaltijd successfully inserted.");
+                        res.status(200).json({
+                            "ID": result.insertId,
+                            "naam": name,
+                            "beschrijving": desc,
+                            "ingredienten": ingr,
+                            "allergie": allergy,
+                            "prijs": price});
+                    }
+                });
+            }
+        });
     },
+
     getMaaltijd(req, res, next){
         console.log('getMaaltijd was called');
         res.status(200).end();
